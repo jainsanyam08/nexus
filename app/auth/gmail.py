8436663d794537fa11a09670285caa60c42ecec9
Sanyam Jain <luvjain2008@gmail.com>
2025-12-31 23:20:51 +0530

    Added Google Auth

--- app/auth/gmail.py
+++ app/auth/gmail.py
@@ -1,47 +1,35 @@
-import os
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
+import os
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+CLIENT_SECRETS_FILE = os.path.join(BASE_DIR, "client_secrets.json")
 
 
 def get_gmail_flow():
-    client_id = os.getenv("25851036802-trob35np94p0ffmgfs53rpvkt3iqrl7i.apps.googleusercontent.com")
-    client_secret = os.getenv("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")
+    if not os.path.exists(CLIENT_SECRETS_FILE):
+        raise RuntimeError("client_secrets.json not found")
 
-    if not client_id or not client_secret:
-        raise RuntimeError("GMAIL_CLIENT_ID or GMAIL_CLIENT_SECRET not set")
-
-    return Flow.from_client_config(
-        {
-            "web": {
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "redirect_uris": [
-                    "http://127.0.0.1:8000/auth/gmail/callback"
-                ],
-            }
-        },
+    flow = Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE,
         scopes=SCOPES,
+        redirect_uri="http://127.0.0.1:8000/auth/gmail/callback",
     )
+    return flow
 
 
 def extract_sender_domains(credentials):
-    """
-    Reads Gmail METADATA ONLY (From headers) and returns sender domains
-    """
     service = build("gmail", "v1", credentials=credentials)
 
-    response = service.users().messages().list(
+    results = service.users().messages().list(
         userId="me",
-        maxResults=50
+        maxResults=25
     ).execute()
 
     domains = set()
 
-    for msg in response.get("messages", []):
+    for msg in results.get("messages", []):
         msg_data = service.users().messages().get(
             userId="me",
             id=msg["id"],
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+print("SYS.PATH:", sys.path)
 
 from app.database import Base, engine
 from app.routes.identity import router
